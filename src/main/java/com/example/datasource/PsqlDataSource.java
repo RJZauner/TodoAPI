5742b1e263b5ac0fb1b307448cf9471d8a103957
RJames Maier <rorymaier@gmail.com>
2020-04-01 17:25:10 +0200

    created DAL-class for psql integration

--- src/main/java/com/example/datasource/PsqlDataSource.java
+++ src/main/java/com/example/datasource/PsqlDataSource.java
@@ -7,13 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MongodbDataSource {
+public class PsqlDataSource {
 
     @Bean
     @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource(){
-        return DataSourceBuilder
-                .create()
+        return DataSourceBuilder.create()
                 .type(HikariDataSource.class)
                 .build();
     }
