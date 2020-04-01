ed805f26da18d69f9b65c6a3f11dc48df50f02d3
roryjames <rorymaier@gmail.com>
2020-04-01 13:44:13 +0200

    initial commit

---
+++ src/main/java/com/example/API/PersonController.java
@@ -0,0 +1,48 @@
+package com.example.API;
+
+import com.example.model.Person;
+import com.example.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping //"/POST"
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping//"/GET"
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id)
+                .orElse( null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
+    }
+
+}
